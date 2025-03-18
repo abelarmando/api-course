0e21f22881db35f08e78591627dfae134086f778
abel armando <abelarmando79@gmail.com>
2025-03-18 14:21:55 +0700

    membenarkan tulisan

--- src/utility/encryptedtoken.js
+++ src/utility/encryptedtoken.js
@@ -1,9 +1,6 @@
 import crypto from "crypto";
 import { CRYPTO_KEY } from "../config/env.js";
 
-export const encryptToken = (no_handphone) => {
-  return crypto
-    .createHmac("sha256", CRYPTO_KEY)
-    .update(no_handphone)
-    .digest("hex");
+export const encryptToken = (email) => {
+  return crypto.createHmac("sha256", CRYPTO_KEY).update(email).digest("hex");
 };
